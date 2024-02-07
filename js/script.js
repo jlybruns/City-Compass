12dd686c90e100618b6dd3f094dcfd3be0d3e934
Joel Bruns <jlybruns@gmail.com>
2024-02-06 21:34:07 -0500

    Added some content to main page & combined  email/password validating.

--- js/script.js
+++ js/script.js
@@ -1,18 +1,28 @@
 //function to validate email checking for @ symbol
 function validateUser(email, password) {
+    if(!email) {
+        throw new Error('You must provide a valid email.');
+        return false;
+    }
     if (!email.includes('@')) {
       throw new Error('Your email must contain the @ symbol');
+      return false;
     }
     if (password.length < 8) {
         throw new Error('Password must be 8 characters or greater');
+        return false;
       }
       if (!/[0-9]/g.test(password)) {
         throw new Error('Password must contain at least one number');
+        return false;
       }
+      return true;
   }
 
-
-  // Task 2: Validate Password. Not sure we want or need this, but it's here to use.
-// function validatePassword(password) {
-    
-//   }
+  function handleSubmit() {
+    let email = document.getElementById("floatingInput").value;
+    let password = document.getElementById("floatingPassword").value;
+    if (validateUser(email, password)){
+        alert('Validation successful');
+    }
+  }
